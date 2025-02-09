99c3b46f6217ba5f1ed5dc444913f3ebeafd4144
HikaroRibeiro <hhakawama@gmail.com>
2025-02-09 16:22:26 -0300

    ✨ Propriedades
    Passar propriedades para objetos.

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,31 @@
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { styles } from './styles'
+
+import { Participant } from '../../components/Participant'
+
+export default function Home() {
+  function handleParticipantAdd() {
+    console.log('Você clicou no botão de add')
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Volta Redonda</Text>
+      <Text style={styles.eventDate}>08 de Fevereiro de 2025</Text>
+      <View style={styles.form}>
+        <TextInput
+          id="txtInput"
+          style={styles.input}
+          placeholder="Insira seu nome aqui."
+          placeholderTextColor="#6B6B6B"
+          keyboardType="email-address"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <Participant name="Hikaro Ribeiro" />
+      <Participant name="Bernardo Ribeiro" />
+    </View>
+  )
+}
